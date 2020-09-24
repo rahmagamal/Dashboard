d75e55294a4355e0b65005630642fb9acbdb184b
rahmagamal <55654504+rahmagamal@users.noreply.github.com>
2020-09-24 02:44:11 +0200

    login

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import login from '../views/login.vue'
+
+import { BootstrapVue, IconsPlugin } from '../../node_modules/bootstrap-vue'
+
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: login
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+ 
 ]
 
 const router = new VueRouter({
---
+++ src/views/login.vue
@@ -0,0 +1,57 @@
+<template>
+<section class="login">
+    <div class="container">
+        <div class="row mt-5">
+              <div class="col-md-5 card">
+                      <h3>Login</h3>
+                      <b-col sm="2">
+                        <label for="input-default">Email:</label>
+                        </b-col>
+                        
+                     
+                        <b-col sm="12" class="mb-3 email">
+                        <b-form-input id="input-default" size="sm" placeholder="Enter your Email"></b-form-input>
+                        </b-col>
+                        <i class="fas fa-envelope emailIcon"></i>
+              
+
+                     
+                        <b-col sm="2">
+                        <label for="input-default">Password:</label>
+                        </b-col>
+                        <b-col sm="12" class="mb-3 password">
+                        <b-form-input id="input-default" size="sm" placeholder="Enter your Password"></b-form-input>
+                        </b-col>
+                         <i class="fas fa-lock passwordIcon"></i>
+
+                     <b-row class="my-1 mb-3">
+                          <b-button  pill variant="outline-secondary">Login</b-button>
+                     </b-row>
+                    <hr style="width:100%; background-color:rgb(148 189 183) ; height:1px">
+                    <h6>DON'T HAVE ANY ACCOUNT ? <a href="#">SIGNUP</a></h6>
+
+               <div>
+  
+        </div>
+              </div>
+        </div>
+    </div>
+</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+body{
+    background-image:url('../assets/images/img5.jpg');
+     height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
